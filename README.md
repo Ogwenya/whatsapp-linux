2c7b34253838ba3395af6cf05fd65a360f1c497d
Ogwenya <emanuelogwenya@outlook.com>
2023-06-01 14:31:52 +0300

    setup

---
+++ README.md
@@ -0,0 +1,4 @@
+## [UNOFFICIAL] WhatsApp-linux
+
+This is a compilled version of [WhatsApp web](https://web.whatsapp.com) for use in linux using [Tauri](https://tauri.dev).
+
---
+++ src-tauri/src/main.rs
@@ -0,0 +1,21 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+use tauri_plugin_window_state;
+use tauri::Manager;
+
+fn main() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_window_state::Builder::default().build())
+        .setup(|app| {
+            let window = app.get_window("main").unwrap();
+            window
+                .eval("window.location.replace('https://web.whatsapp.com/');")
+                .unwrap();
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
